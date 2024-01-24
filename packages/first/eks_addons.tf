faded05607f36f096b92780cda8f2caf3f318f57
Veselin Hristov <veselin.hristov@softwareone.com>
2024-01-24 12:55:11 +0200

    add example chart details for CAS

--- packages/first/eks_addons.tf
+++ packages/first/eks_addons.tf
@@ -8,9 +8,18 @@ module "eks-addons" {
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
-  # EKS Blueprints Add-ons:
   # Cluster Auto-scaler
   enable_cluster_autoscaler = true
+  cluster_autoscaler        = {
+    namespace     = "kube-system"
+    name          = "cluster-autoscaler"
+    description   = "A Helm chart to deploy cluster-autoscaler"
+    chart         = "cluster-autoscaler"
+    chart_version = "9.29.1"
+    repository    = "https://kubernetes.github.io/autoscaler"
+    set           = []
+    set_sensitive = []
+  }
 
   # AWS LB Controller
   enable_aws_load_balancer_controller = true
