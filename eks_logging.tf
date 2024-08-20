01f35d4c4d183e61e79612779bc6243ced4b3c33
Ruslan Gladov <ruslan.gladov@softwareone.com>
2024-08-20 13:46:57 +0300

    eks manifests

--- eks_logging.tf
+++ eks_logging.tf
@@ -1,17 +1,17 @@
-module "eks_logging" {
-  source = "./modules/terraform-modules-eks-logging-fluentbit"
-  fluentbit_cluster_info_configs = {
-    "logs.region"  = var.region,
-    "cluster.name" = module.eks.cluster_id,
-    "http.server"  = "On",
-    "http.port"    = "2020",
-    "read.head"    = "Off",
-    "read.tail"    = "On"
-  }
-  fluentbit_image        = "public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"
-  namespace_name         = "amazon-cloudwatch"
-  logs_destination_store = "cloudwatch"
-  prefix_name            = "fluent-bit"
-  eks_oidc_issuer_url    = local.eks_oidc_issuer_url
-  eks_oidc_provider_arn  = local.eks_oidc_provider_arn
-}
+#module "eks_logging" {
+#  source = "./modules/terraform-modules-eks-logging-fluentbit"
+#  fluentbit_cluster_info_configs = {
+#    "logs.region"  = var.region,
+#    "cluster.name" = module.eks.cluster_id,
+#    "http.server"  = "On",
+#    "http.port"    = "2020",
+#    "read.head"    = "Off",
+#    "read.tail"    = "On"
+#  }
+#  fluentbit_image        = "public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"
+#  namespace_name         = "amazon-cloudwatch"
+#  logs_destination_store = "cloudwatch"
+#  prefix_name            = "fluent-bit"
+#  eks_oidc_issuer_url    = local.eks_oidc_issuer_url
+#  eks_oidc_provider_arn  = local.eks_oidc_provider_arn
+#}
